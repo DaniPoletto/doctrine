f1d4e6a6df99413e733f0293a1615496b97cf55a
Daniela Poletto <danielapoletto_bep@hotmail.com>
2022-04-12 21:34:13 -0300

    Criando classe de criação de gerenciador de entidade | Creating Entity Manager Factory

---
+++ src/Helper/EntityManagerFactory.php
@@ -0,0 +1,35 @@
+<?php
+namespace Alura\Doctrine\Helper;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Setup;
+
+//Classe para criar um gerenciador de entidades
+class EntityManagerFactory{
+    //exemplo de anotação informando que a função retornará esse tipo
+    /**
+     * @return EntityManagerInterface
+     */
+    public function getEntityManager():EntityManagerInterface
+    {
+        $rootDir = __DIR__ . '/../..';
+        //cria as configurações que o doctrine precisa através de anotações
+        //busca apartir do directory src e com mode de desenvolvimento = true
+        $config = Setup::createAnnotationMetadataConfiguration(
+            [$rootDir . '/src'],
+            true
+        );
+
+        //informações de conexão
+        $connection = [
+            'driver' => 'pdo_sqlite',
+            'path' => $rootDir . '/var/data/banco.sqlite'
+        ];
+
+        //cria um gerenciador de entidades
+        return EntityManager::create($connection, $config);
+
+    }
+}
+
+?>
