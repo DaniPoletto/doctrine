d88b8000892c4782f7acd31ab79fdab2abac3f21
Daniela Poletto <danielapoletto_bep@hotmail.com>
2022-04-13 21:14:14 -0300

    salvar aluno no banco | save Student in database

---
+++ commands/criar-aluno.php
@@ -0,0 +1,19 @@
+<?php
+
+use Alura\Doctrine\Entity\Aluno;
+use Alura\Doctrine\Helper\EntityManagerFactory;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$aluno = new Aluno();
+$aluno->setNome("Daniela");
+
+$entityManagerFactory = new EntityManagerFactory();
+$entityManager = $entityManagerFactory->getEntityManager();
+
+//começa a 'observar' esse aluno
+$entityManager->persist($aluno);
+$aluno->setNome("Dani");
+
+//metódo que efetiva o que foi manipulado no banco
+$entityManager->flush();
