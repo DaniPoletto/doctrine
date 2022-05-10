218ff31f979e380dc04b3ccee55bf883c243169c
Daniela Poletto <danielapoletto_bep@hotmail.com>
2022-05-09 21:11:26 -0300

    um pra muitos, migrations - one to many, migrations

--- commands/buscar-alunos.php
+++ commands/buscar-alunos.php
@@ -1,5 +1,6 @@
 <?php
 use Alura\Doctrine\Entity\Aluno;
+use Alura\Doctrine\Entity\Telefone;
 use Alura\Doctrine\Helper\EntityManagerFactory;
 
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -13,29 +14,37 @@ $alunoRepository = $entityManager->getRepository(Aluno::class);
 $alunoList = $alunoRepository->findAll();
 
 foreach ($alunoList as $aluno) {
-    echo "ID: {$aluno->getId()} \nNome: {$aluno->getNome()}\n\n";
+    $telefones = $aluno
+                ->getTelefones()
+                ->map(function(Telefone $telefone){
+                    return $telefone->getNumero();
+                })
+                ->toArray();
+    echo "ID: {$aluno->getId()} \nNome: {$aluno->getNome()}\n";
+    echo "Telefones: ".implode(', ', $telefones);
+    echo "\n\n";
 }
 
-$nico = $alunoRepository->find(6);
-echo $nico->getNome();
+// $nico = $alunoRepository->find(6);
+// echo $nico->getNome();
 
-$Ana = $alunoRepository->findBy([
-    'nome' => 'Ana'
-]);
+// $Ana = $alunoRepository->findBy([
+//     'nome' => 'Ana'
+// ]);
 
-var_dump($Ana);
+// var_dump($Ana);
 
-//retorna só o objeto do tipo aluno
-$Ana = $alunoRepository->findOneBy([
-    'nome' => 'Ana'
-]);
+// //retorna só o objeto do tipo aluno
+// $Ana = $alunoRepository->findOneBy([
+//     'nome' => 'Ana'
+// ]);
 
-var_dump($Ana);
+// var_dump($Ana);
 
-//Para buscar apenas 2 entidades, ordenadas pelo campo nome de forma crescente, começando a partir do 3 resultado
-$teste = $alunoRepository->findBy([], ['nome' => 'ASC'], 2, 3);
+// //Para buscar apenas 2 entidades, ordenadas pelo campo nome de forma crescente, começando a partir do 3 resultado
+// $teste = $alunoRepository->findBy([], ['nome' => 'ASC'], 2, 3);
 
-var_dump($teste);
+// var_dump($teste);
 
 /*
 public function findBy(
--- src/Entity/Aluno.php
+++ src/Entity/Aluno.php
@@ -1,6 +1,9 @@
 <?php
 namespace Alura\Doctrine\Entity;
 
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+
 /**
  * @Entity
  */
@@ -16,6 +19,14 @@ class Aluno
      * @Column(type="string")
      */
     private $nome;
+    /**
+     * @OneToMany(targetEntity="Telefone", mappedBy="aluno", cascade={"remove", "persist"})
+     */
+    private $telefones;
+
+    public function __construct(){
+        $this->telefones = new ArrayCollection();
+    }
 
     public function getId() :string
     {
@@ -31,5 +42,17 @@ class Aluno
     {
         $this->nome = $nome;
         return $this;
+    }
+
+    public function addTelefone(Telefone $telefone)
+    {
+        $this->telefones->add($telefone);
+        $telefone->setAluno($this);
+        return $this;
+    }
+
+    public function getTelefones():Collection
+    {
+        return $this->telefones;
     }
 }
